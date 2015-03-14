2c7f530430118f3514d84e58a7a09d76b06264a0
Luiz Fernando L R Ferreira <lewisferreira@users.noreply.github.com>
2015-03-14 18:03:17 -0300

    Create Ex2_par.py

---
+++ Ex2_par.py
@@ -0,0 +1,12 @@
+def entra():
+	return int(input("Digite o numero: "))
+
+def checa_par(x):
+	if x % 2 == 0:
+		print("o numero e par!")
+	else:
+		print("o numero e impar!")
+
+a = entra()
+checa_par(a)
+input()

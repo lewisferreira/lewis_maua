6a0c7aba5f8f1e7e4fa7a0279684d89330e6b8d9
Luiz Fernando L R Ferreira <lewisferreira@users.noreply.github.com>
2015-03-14 18:02:42 -0300

    Create Ex1_media3.py

---
+++ Ex1_media3.py
@@ -0,0 +1,12 @@
+def soma(a,b): 
+	return a+b
+
+def entra():
+	return int(input("Digite a nota: "))
+
+a = entra()
+b = entra()
+c = entra()
+
+print(soma(soma(a, b), c)/3)	#reaproveitando a funÃ§Ã£o de soma criada antes
+input()

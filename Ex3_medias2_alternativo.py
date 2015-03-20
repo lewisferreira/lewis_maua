7d5ab4b5d4f9fdc446bc289fdfd413c9eb8d92d2
Luiz Fernando L R Ferreira <lewisferreira@users.noreply.github.com>
2015-03-20 11:29:36 -0300

    Create Ex3_medias2_alternativo.py

---
+++ Ex3_medias2_alternativo.py
@@ -0,0 +1,13 @@
+i = 0
+S = 0
+while True:
+	nota = float(input("Digite a nota: "))
+	if nota < 0:
+		break
+	S += nota
+	i += 1
+if i != 0:
+	print("a media eh: {0}".format(S/i))
+else:
+	print("nao foi digitado nenhuma nota...")
+input()

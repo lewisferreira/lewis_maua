1f0e2afd31c993ae8fc37381bffccd36991a0068
Luiz Fernando L R Ferreira <lewisferreira@users.noreply.github.com>
2015-03-27 12:11:36 -0300

    Create Trabalho_pratico_1_ex1.py

---
+++ Trabalho_pratico_1_ex1.py
@@ -0,0 +1,13 @@
+def distancia(p1,p2):
+	return ( ( float(p1[0]) - float(p2[0]) )**2 + ( float(p1[1]) - float(p2[1]) )**2 )**(1/2)
+
+def dig():
+	return input("Digite o ponto no seguinte formato 'x,y': ").split(',')
+
+p1 = dig()
+p2 = dig()
+
+print("x1 = {0}\ty1= {1}\nx2 = {2}\ty2 = {3}\ndistancia = {4}".\
+	format( p1[0], p1[1], p2[0], p2[1], distancia(p1,p2) ))
+
+input()

a6488d921760d75dcab8a90e14543c93e5acf82d
Sheldon Demario <sheldon@telefonicabeta.com>
2015-04-09 11:10:53 -0300

    Correcao Ex1 ao Ex4

--- Ex1_media3.py
+++ Ex1_media3.py
@@ -8,5 +8,8 @@ a = entra()
 b = entra()
 c = entra()
 
-print(soma(soma(a, b), c)/3)	#reaproveitando a funÃ§Ã£o de soma criada antes
+print(soma(soma(a, b), c)/3)	#reaproveitando a funcao de soma criada antes
 input()
+
+# Nota: 0.9
+# Comentario: Bom uso de funcoes, entretanto input desnecessario no final

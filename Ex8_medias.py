16803a52f33656962e4f56da5edf0db397198c53
Luiz Fernando L R Ferreira <lewisferreira@users.noreply.github.com>
2015-04-10 11:05:01 -0300

    Create Ex8_medias.py

---
+++ Ex8_medias.py
@@ -0,0 +1,17 @@
+notas = []
+S = 0
+while True:
+	try:
+		nota = float(raw_input("Digite a nota: "))
+	except:
+		print("voce nao digitou um numero")
+		continue
+	if nota < 0:
+		break
+	notas.append(nota)
+for i in notas:
+	S += i
+if len(notas) != 0:
+	print("a media eh: {0}\na maior nota foi: {1}".format(S/len(notas), max(notas) ))
+else:
+	print("nao foi digitado nenhuma nota...")
